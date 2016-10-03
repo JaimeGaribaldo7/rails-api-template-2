7e6789b7cd782a292c45ea1cfe5249f7234e7532
JaimeGaribaldo7 <jaimegaribaldo753@gmail.com>
2016-10-03 11:52:39 -0400

    Before nuke_pave

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'CSV'
+namespace :db do
+  desc 'Load example data from db/examples.rb'
+  task examples: :environment do
+    require_relative '../../db/examples'
+  end
+
+  desc 'Fill the pokemons table with pokemons data'
+  task pokemons: :environment do
+    Pokemon.transaction do
+      CSV.foreach(Rails.root + 'data/pokemon.csv',
+                  headers: true) do |row|
+        pokemon = row.to_hash
+        next if Pokemon.exists? pokemon
+        Pokemon.create!(pokemon)
+      end
+    end
+  end
+
+  unless Rails.env == 'production'
+    desc 'Drop and setup the development database with examples'
+    task nuke_pave:
+    %w(environment db:drop db:create db:migrate db:seed db:examples db:pokemons) do
+      puts "Nuke and pave of #{Rails.env} complete."
+    end
+  end
+end
