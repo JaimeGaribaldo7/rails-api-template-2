76a128e7e1b5f0586de4dc26e58ed3709bd83edb
JaimeGaribaldo7 <jaimegaribaldo753@gmail.com>
2016-10-04 16:12:22 -0400

    3rd try

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module PokePC
     # config.i18n.load_path +=
     #   Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-# http://localhost:3000
+    # http://localhost:3000
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
