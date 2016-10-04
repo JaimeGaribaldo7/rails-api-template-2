ca22db5a2a1b5e4aac6c891f621fa54408c67235
JaimeGaribaldo7 <jaimegaribaldo753@gmail.com>
2016-10-04 15:33:12 -0400

    Fixed CSV file import.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'CSV'
-Pokemon.transaction do
-  CSV.foreach(Rails.root + 'data/pokemon.csv', headers: true, header_converters: -> (h) { h.downcase.to_sym }) do |pokemon|
-    p pokemon
-    @pokemons << Pokemon.new(pokemon.to_hash)
-    Pokemon.create(id: hs[:id], name: hs[:name], number: hs[:number].to_s, pokemontype: hs[:pokemontype])
-  end
-end
+# require 'CSV'
+# Pokemon.transaction do
+#   CSV.foreach('data/pokemon.csv', headers: true, header_converters: -> (h) { h.downcase.to_sym }) do |pokemon|
+#     p pokemon
+#     Pokemon.create(id: pokemon[:id], name: pokemon[:name], number: pokemon[:number].to_s, pokemontype: pokemon[:pokemontype])
+#   end
+# end
 
 #   unless Rails.env == 'production'
 #     desc 'Drop and setup the development database with examples'
