937c560c10452dd3a279f6fbda60ace5a8db2cab
JaimeGaribaldo7 <jaimegaribaldo753@gmail.com>
2016-10-04 15:56:05 -0400

    Changed CLIENT ORIGIN in application.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   patch '/change-password/:id' => 'users#changepw'
   get '/poke-pc/:id' => 'pokemons#index'
   get '/teams/:id' => 'teams#index'
+
   patch '/teams/:id' => 'teams#update'
   post '/teams/:id' => 'teams#create'
+
   resources :users, only: [:index, :show]
   resources :pokemons, except: [:new, :edit]
   resources :teams, except: [:new, :edit]
