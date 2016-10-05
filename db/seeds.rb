b31d180326689654f9469ae221cba63a7493b314
JaimeGaribaldo7 <jaimegaribaldo753@gmail.com>
2016-10-04 20:42:53 -0400

    change CSV to csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'CSV'
+require 'csv'
 Pokemon.transaction do
   CSV.foreach('data/pokemon.csv', headers: true, header_converters: -> (h) { h.downcase.to_sym }) do |pokemon|
     p pokemon
--- scripts/users.sh
+++ scripts/users.sh
@@ -2,3 +2,7 @@
 
 curl --include --request GET http://localhost:3000/users \
   --header "Authorization: Token token=$TOKEN"
+
+
+  curl --include --request GET https://dry-mesa-20099.herokuapp.com/pokemons \
+    --header "Authorization: Token token=$TOKEN"
